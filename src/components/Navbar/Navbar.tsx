6f86ddde52aeaa50d3ac0a139427a1a7dbbe8a44
Mykola-Maik <mykoluamaik@gmail.com>
2024-12-28 21:49:56 +0200

    setup project

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+export const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
+      <h3 className="font-bold">Github Search</h3>
+      <span className="font-bold">Github</span>
+    </nav>
+  );
+};
