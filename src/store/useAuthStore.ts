e10d3cbe55fd68fe4a7f09cf2c7f322db1ceb001
Mykola-Maik <mykoluamaik@gmail.com>
2025-01-22 17:27:47 +0200

    quick fix

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -13,10 +13,7 @@ import type {
 } from "@/types";
 import { io } from "socket.io-client";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_BACKEND_URL
-    : "/api";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const useAuthStore = create<AuthState>((set, get) => ({
   authUser: null,
